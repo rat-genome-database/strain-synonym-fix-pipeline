f379423a817b3cb977384735f95d2c8fc5659e35
tutajm <mtutaj@mcw.edu>
2021-12-20 15:25:36 -0600

    upgraded log4j to avoid zero day exploit

--- src/main/dist/run_strainsynonymfix.sh
+++ src/main/dist/run_strainsynonymfix.sh
@@ -2,7 +2,6 @@
 # "||" sign at the query of Statement stmt needs to be changed into "," for the second run 
 # Created by : Burcu Bakir 
 # Date: 04/27/2009
-# Modified by Marek Tutaj, 3/12/2012 -- upgraded to latest rgdcore and jars
 #
 . /etc/profile
 APPNAME=StrainSynonymFix
